8adc9c9b25f02a9452f029d17098be3159419325
MyChi <mychikhtn@gmail.com>
2026-05-13 15:37:42 +0700

    Xử lý logic trang login

--- Shope/ShopeProject/src/pages/Login/Login.tsx
+++ Shope/ShopeProject/src/pages/Login/Login.tsx
@@ -1,43 +1,83 @@
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { data, Link } from 'react-router-dom'
+import Input from '../../components/Input'
+import { schema, type Schema } from '../../utils/rules'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useMutation } from '@tanstack/react-query'
+import { login } from '../../apis/auth.api'
+import { isAxiosUnprocessableEntityError } from '../../utils/utils'
+import type { ResponseApi } from '../../types/utils.type'
+
+type FormData = Omit<Schema, 'confirm_password'>
+const loginSchema = schema.omit(['confirm_password'])
 
 export default function Login() {
   const {
     register,
+    setError,
     handleSubmit,
-    formState: { errors }
-  } = useForm()
+    formState: { errors },
+    watch
+  } = useForm<FormData>({
+    resolver: yupResolver(loginSchema)
+  })
 
-  const onSubmit = (data) => {
-    console.log(data)
-  }
+  const loginMutation = useMutation({
+    mutationFn: (body: Omit<FormData, 'confirm_password'>) => login(body)
+  })
+
+  const onSubmit = handleSubmit((data) => {
+    console.log('data login', data)
+    loginMutation.mutate(data, {
+      onSuccess: (data) => {
+        console.log('Đăng nhập thành công', data)
+      },
+      onError: (error) => {
+        if (isAxiosUnprocessableEntityError<ResponseApi<FormData>>(error)) {
+          const formError = error.response?.data.data
+          console.log('formError', formError)
+          if (formError) {
+            Object.keys(formError).forEach((key) => {
+              setError(key as keyof FormData, {
+                message: formError[key as keyof FormData],
+                type: 'Server'
+              })
+            })
+          }
+        }
+      }
+    })
+  })
+
+  const value = watch()
+  console.log('value', value)
 
   return (
     <div className='bg-orange'>
       <div className='container'>
         <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <form className='rounded bg-white p-10 shadow-sm' onSubmit={handleSubmit(onSubmit)}>
+            <form className='rounded bg-white p-10 shadow-sm' onSubmit={onSubmit} noValidate>
               <div className='text-2xl'>Đăng nhập</div>
-              <div className='mt-8'>
-                <input
-                  type='email'
-                  name='email'
-                  placeholder='Email'
-                  className='w-full rounded-sm border border-gray-300
-                            p-3 outline-none focus:broder-gray-500 focus:shadow-sm'
-                />
-                <div className='mt-1 min-h-[1rem] text-sm text-red-600'></div>
-              </div>
-              <div className='mt-3'>
-                <input
-                  type='password'
-                  name='password'
-                  className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
-                  placeholder='Password'
-                />
-                <div className='mt-1 min-h-[1rem] text-sm text-red-600'></div>
-              </div>
+              <Input
+                name='email'
+                register={register}
+                type='email'
+                className='mt-8'
+                errorMessage={errors.email?.message}
+                placeholder='Email'
+              />
+
+              <Input
+                name='password'
+                register={register}
+                type='password'
+                className='mt-2'
+                errorMessage={errors.password?.message}
+                placeholder='Password'
+                autoComplete='on'
+              />
+
               <div className='mt-3'>
                 <button
                   type='submit'
