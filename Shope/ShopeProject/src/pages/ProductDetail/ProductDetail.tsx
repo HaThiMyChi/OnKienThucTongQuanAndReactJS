04bc8a61cab56cb87c29ca27de761fe06fb19f1c
MyChi <mychikhtn@gmail.com>
2026-05-28 16:25:09 +0700

    Code logic di chuột thì zoom ảnh

--- Shope/ShopeProject/src/pages/ProductDetail/ProductDetail.tsx
+++ Shope/ShopeProject/src/pages/ProductDetail/ProductDetail.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import productApi from '../../apis/product.api'
 import ProductRaiting from '../../components/ProductRaiting'
@@ -21,6 +21,8 @@ export default function ProductDetail() {
 
   const product = productDetailData?.data.data
   console.log('product data', product)
+
+  const imageRef = useRef<HTMLImageElement>(null)
 
   const currentImages = useMemo(
     () => (product ? product.images.slice(...currentIndexImages) : []),
@@ -54,6 +56,37 @@ export default function ProductDetail() {
     setActiveImage(img)
   }
 
+  const handleZoom = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    // can biet khung hinh dang hien thi nam o dau tren trinh duyet va kich thuoc cua no la bao nhieu
+    // rect sẽ cho chúng ta biết width, height, và tọa độ x, y của khung chứa ảnh.
+    const rect = event.currentTarget.getBoundingClientRect()
+    console.log('rect image', rect)
+
+    const image = imageRef.current as HTMLImageElement
+
+    // naturalWidth và naturalHeight là kích thước thực tế của file ảnh
+    const { naturalHeight, naturalWidth } = image
+    // Cách 1: Lấy offsetX, offsetY đơn giản khi chúng ta đã xử lý được bubble event
+    // const { offsetX, offsetY } = event.nativeEvent
+
+    // Cách 2: Lấy offsetX, offsetY khi chúng ta không xử lý được bubble event
+    const offsetX = event.pageX - (rect.x + window.scrollX)
+    const offsetY = event.pageY - (rect.y + window.scrollY)
+
+    const top = offsetY * (1 - naturalHeight / rect.height)
+    const left = offsetX * (1 - naturalWidth / rect.width)
+
+    image.style.width = naturalWidth + 'px'
+    image.style.height = naturalHeight + 'px'
+    image.style.maxWidth = 'unset'
+    image.style.top = top + 'px'
+    image.style.left = left + 'px'
+  }
+
+  const handleRemoveZoom = () => {
+    imageRef.current?.removeAttribute('style')
+  }
+
   if (!product) return null
 
   return (
@@ -62,11 +95,16 @@ export default function ProductDetail() {
         <div className='bg-white p-4 shadow'>
           <div className='grid grid-cols-12 gap-9'>
             <div className='col-span-5'>
-              <div className='relative w-full pt-[100%] shadow'>
+              <div
+                className='relative w-full pt-[100%] shadow cursor-zoom-in overflow-hidden'
+                onMouseMove={handleZoom}
+                onMouseLeave={handleRemoveZoom}
+              >
                 <img
                   src={activeImage}
                   alt={product.name}
                   className='absolute left-0 top-0 h-full w-full bg-white object-cover'
+                  ref={imageRef}
                 />
               </div>
               <div className='relative mt-4 grid grid-cols-5 gap-1'>
