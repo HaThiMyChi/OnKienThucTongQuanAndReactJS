eb7183b57b75a0713c2e6f42eae450892f4721df
MyChi <mychikhtn@gmail.com>
2026-06-22 19:17:42 +0700

    Hiển thị data profile lên form

--- Shope/ShopeProject/src/apis/user.api.ts
+++ Shope/ShopeProject/src/apis/user.api.ts
@@ -33,3 +33,5 @@ const userApi = {
     })
   }
 }
+
+export default userApi
--- Shope/ShopeProject/src/utils/rules.ts
+++ Shope/ShopeProject/src/utils/rules.ts
@@ -15,11 +15,11 @@ export const getRules = (getValues?: UseFormGetValues<any>): Rules => ({
     },
     maxLength: {
       value: 160,
-      message: 'Độ dài từ 5 - 160 kí tự'
+      message: 'Độ dài từ 5 - 160 ký tự'
     },
     minLength: {
       value: 5,
-      message: 'Độ dài từ 5 - 160 kí tự'
+      message: 'Độ dài từ 5 - 160 ký tự'
     }
   },
   password: {
@@ -29,11 +29,11 @@ export const getRules = (getValues?: UseFormGetValues<any>): Rules => ({
     },
     maxLength: {
       value: 160,
-      message: 'Độ dài từ 6 - 160 kí tự'
+      message: 'Độ dài từ 6 - 160 ký tự'
     },
     minLength: {
       value: 6,
-      message: 'Độ dài từ 6 - 160 kí tự'
+      message: 'Độ dài từ 6 - 160 ký tự'
     }
   },
   confirm_password: {
@@ -43,11 +43,11 @@ export const getRules = (getValues?: UseFormGetValues<any>): Rules => ({
     },
     maxLength: {
       value: 160,
-      message: 'Độ dài từ 6 - 160 kí tự'
+      message: 'Độ dài từ 6 - 160 ký tự'
     },
     minLength: {
       value: 6,
-      message: 'Độ dài từ 6 - 160 kí tự'
+      message: 'Độ dài từ 6 - 160 ký tự'
     },
     validate:
       typeof getValues === 'function'
@@ -70,18 +70,18 @@ export const schema = yup.object({
     .string()
     .required('Email là bắt buộc')
     .email('Email không đúng định dạng')
-    .min(5, 'Độ dài từ 5 - 160 kí tự')
-    .max(160, 'Độ dài từ 5 - 160 kí tự'),
+    .min(5, 'Độ dài từ 5 - 160 ký tự')
+    .max(160, 'Độ dài từ 5 - 160 ký tự'),
   password: yup
     .string()
     .required('Password là bắt buộc')
-    .min(6, 'Độ dài từ 6 - 160 kí tự')
-    .max(160, 'Độ dài từ 6 - 160 kí tự'),
+    .min(6, 'Độ dài từ 6 - 160 ký tự')
+    .max(160, 'Độ dài từ 6 - 160 ký tự'),
   confirm_password: yup
     .string()
     .required('Nhập lại password là bắt buộc')
-    .min(6, 'Độ dài từ 6 - 160 kí tự')
-    .max(160, 'Độ dài từ 6 - 160 kí tự')
+    .min(6, 'Độ dài từ 6 - 160 ký tự')
+    .max(160, 'Độ dài từ 6 - 160 ký tự')
     .oneOf([yup.ref('password')], 'Nhập lại password không khớp'),
   price_min: yup.string().test({
     name: 'price-not-allowed',
@@ -95,6 +95,19 @@ export const schema = yup.object({
   }),
   name: yup.string().trim().required('Tên sản phẩm là bắt buộc')
 })
+
+export const userSchema = yup.object({
+  name: yup.string().max(160, 'Độ dài tối đa là 160 ký tự'),
+  phone: yup.string().max(20, 'Độ dài tối đa là 20 ký tự'),
+  address: yup.string().max(160, 'Độ dài tối đa là 160 ký tự'),
+  avatar: yup.string().max(1000, 'Độ dài tối đa là 1000 ký tự'),
+  date_of_birth: yup.date().max(new Date(), 'Hãy chọn một ngày trong quá khứ'),
+  password: schema.fields['password'],
+  new_password: schema.fields['password'],
+  confirm_password: schema.fields['confirm_password']
+})
+
+export type UserSchema = yup.InferType<typeof userSchema>
 
 // Yup - tự động infer type
 // Tự động biết fields và types!
