a4da9a0585ae4cc5b07a82784254726a36a66c1b
MyChi <mychikhtn@gmail.com>
2026-06-27 17:17:02 +0700

    Thêm 404 page

--- Shope/ShopeProject/src/useRouteElements.tsx
+++ Shope/ShopeProject/src/useRouteElements.tsx
@@ -14,6 +14,7 @@ import CartLayout from './layouts/CartLayout'
 import UserLayout from './pages/User/layouts/UserLayout'
 import ChangePassword from './pages/User/pages/ChangePassword'
 import HistoryPurchase from './pages/User/pages/HistoryPurchase'
+import NotFound from './pages/NotFound'
 
 function ProtectedRouter() {
   const { isAuthenticated } = useContext(AppContext)
@@ -101,6 +102,14 @@ export default function useRouteElements() {
       element: (
         <MainLayout>
           <ProductList />
+        </MainLayout>
+      )
+    },
+    {
+      path: '*',
+      element: (
+        <MainLayout>
+          <NotFound />
         </MainLayout>
       )
     }
